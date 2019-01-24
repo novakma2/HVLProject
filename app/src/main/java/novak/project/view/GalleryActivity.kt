2e83296c3b0ad42fa231365b8dff421c81e92fb2
novak <novakma2@uhk.cz>
2019-01-24 10:36:00 +0100

    its not working, but yeah

--- app/src/main/java/novak/project/view/GalleryActivity.kt
+++ app/src/main/java/novak/project/view/GalleryActivity.kt
@@ -4,16 +4,19 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import novak.project.R
+import novak.project.model.Person
 
 class GalleryActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_gallery)
-    }
+        val persons: Array<Person> = intent.extras.get("DB") as Array<Person>
+        Toast.makeText(this, persons.get(0).surname, Toast.LENGTH_LONG).show()
 
+    }
     fun onClickBack(v: View) {
         val intent = Intent(this, MainActivity::class.java).apply {
         }
--- app/src/main/java/novak/project/view/MainActivity.kt
+++ app/src/main/java/novak/project/view/MainActivity.kt
@@ -10,23 +10,23 @@ import novak.project.controller.AppDatabase
 import novak.project.model.Person
 
 class MainActivity : Activity() {
-
+    lateinit var finalPersons: Array<Person>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val db = Room.databaseBuilder(
             applicationContext,
-            AppDatabase::class.java, "database-name"
+            AppDatabase::class.java, "db"
         ).build()
-        var persons: Array<Person> = arrayOf(Person(0, "Marta", "Novak"), Person(1, "Peter"))
+        var persons: Array<Person> = arrayOf(Person(0, "Marta", "Novak"), Person(0, "Peter"))
         db.personsDao().init(persons)
+        //finalPersons = db.personsDao().getPersons()
         setContentView(R.layout.activity_main)
     }
 
 
     fun onClickGallery(v: View) {
-        val intent = Intent(this, GalleryActivity::class.java).apply {
-
-        }
+        val intent = Intent(this, GalleryActivity::class.java)
+        intent.putExtra("DB", finalPersons)
         startActivity(intent)
     }
 
